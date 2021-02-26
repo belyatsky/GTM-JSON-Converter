59f63159b389efc4bea6fbf02f04fddd85f948b1
belyatsky <40917548+belyatsky@users.noreply.github.com>
2021-02-26 22:08:53 +0100

    Update converter.py

--- converter.py
+++ converter.py
@@ -7,14 +7,18 @@ import pandas as pd
 with open('666.json','r') as f:
     data = json.loads(f.read())
 
-elements = ["tag", "trigger", "variable", "builtInVariable", "folder", "zone", "customTemplate"]
-tabs = []
-
-for el in elements:
-    if el in data["containerVersion"].keys():
-        tabs.append(el)
+elements = {"tag": "Tags",
+            "trigger": "Triggers",
+            "variable": "User-Defined Variables",
+            "builtInVariable": "Built-In Variables",
+            "folder": "Folders",
+            "zone": "Zones",
+            "customTemplate": "Templates"}
 
 # Change the name of the final xlsx file, if necessary
 with pd.ExcelWriter('output-GTM.xlsx') as writer:
-    for i in tabs:
-        pd.json_normalize(data["containerVersion"][i]).to_excel(writer, sheet_name=i, index=False)
+    for i in elements:
+        if i in data["containerVersion"].keys():
+            pd.json_normalize(data["containerVersion"][i]).to_excel(writer, sheet_name=elements[i], index=False)
+
+print("Done.")
